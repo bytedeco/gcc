17df99d97cec3b83443d363e4dad7d33408c47ce
supergrecko <me@supergrecko.com>
2021-01-18 12:59:17 +0100

    Add support for Linux under x86-64
    This patch adds Linux builds for x86-64

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+GCC_VERSION=10.2.0
+
+mkdir -p build/$PLATFORM
+cd build/$PLATFORM
+
+if [ ! -e "gcc-$GCC_VERSION.tar.gz" ]; then
+  curl -L -o "gcc-$GCC_VERSION.tar.gz" "https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
+fi
+
+rm -rf build install
+tar --totals -xf "gcc-$GCC_VERSION.tar.gz"
+mkdir build install
+
+GCC_INSTALL_PREFIX=$(pwd)/install
+
+case $PLATFORM in
+  linux-x86_64)
+    export CC="gcc -m64 -fPIC"
+    export CXX="g++ -m64 -fPIC"
+
+    cd gcc-$GCC_VERSION
+    ./contrib/download_prerequisites
+    cd ../build
+
+    ../gcc-$GCC_VERSION/configure \
+      --prefix=$GCC_INSTALL_PREFIX \
+      --enable-checking=release \
+      --enable-languages=jit \
+      --enable-host-shared \
+      --disable-bootstrap \
+      --disable-multilib \
+      --disable-nls
+    make -j $MAKEJ
+    make install
+    ;;
+  *)
+    echo "Platform \"$PLATFORM\" is not supported."
+    exit 1
+    ;;
+esac
+
+cd ../../..
