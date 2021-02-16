53f94acecdda2c9ac8b2468fae2c5469bfb85871
supergrecko <me@supergrecko.com>
2021-02-16 16:13:33 +0100

    Add support for Linux under armhf

--- build.sh
+++ build.sh
@@ -17,6 +17,25 @@ mkdir build install
 GCC_INSTALL_PREFIX=$(pwd)/install
 
 case $PLATFORM in
+  linux-armhf)
+    export CC="arm-linux-gnueabihf-gcc -fPIC"
+    export CXX="arm-linux-gnueabihf-g++ -fPIC"
+
+    cd gcc-$GCC_VERSION
+    ./contrib/download_prerequisites
+    cd ../build
+
+    ../gcc-$GCC_VERSION/configure \
+      --prefix=$GCC_INSTALL_PREFIX \
+      --host=arm-linux-gnueabihf \
+      --target=arm-linux-gnueabihf \
+      --enable-checking=release \
+      --enable-languages=jit \
+      --enable-host-shared \
+      --disable-bootstrap \
+      --disable-multilib \
+      --disable-nls
+    ;;
   linux-x86_64)
     export CC="gcc -m64 -fPIC"
     export CXX="g++ -m64 -fPIC"
